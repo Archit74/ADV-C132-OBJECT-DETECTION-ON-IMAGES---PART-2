374a26c1809eaec89c106cf5998a689ae1ae5e99
Archit74 <81956741+Archit74@users.noreply.github.com>
2022-05-25 21:09:57 +0530

    Add files via upload

---
+++ basket.js
@@ -0,0 +1,35 @@
+function basket_back()
+{
+    window.location="index.html";
+}
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+img = loadImage();
+}
+
+
+function setup() {
+canvas = createCanvas(640, 420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("basket_status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+console.log("Model Loaded!")
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+console.log(error);
+}
+console.log(results);
+objects = results;
+}
---
+++ bottle.js
@@ -0,0 +1,36 @@
+function bottle_back()
+{
+    window.location="index.html";
+}
+
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+img = loadImage();
+}
+
+
+function setup() {
+canvas = createCanvas(640, 420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("bottle_status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+console.log("Model Loaded!")
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+console.log(error);
+}
+console.log(results);
+objects = results;
+}
